88f252a9cd5673baaac559ef3393f69c0cc9a24b
Ritiksharmaji <119768600+Ritiksharmaji@users.noreply.github.com>
2023-12-15 13:59:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+    $('.featured-carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
